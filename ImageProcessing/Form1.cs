ffc50c843c9e245936039135680b4e339170c919
Clorkies <clarkisnoob434@gmail.com>
2025-09-24 00:06:25 +0800

    More functionalities implemented (Part 1 and partially Part 2)

--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -14,12 +14,15 @@ namespace ImageProcessing
     {
         int index;
         OpenFileDialog ofile;
+        private bool isAdvancedFilter = false;
+        private string currentAdvancedFilter = "";
 
         public Form1()
         {
             InitializeComponent();
-            comboBox1.Items.Add("-- Select Filter --");
-            comboBox1.SelectedIndex = 5;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.AutoSize = false;
+            button4.Visible = false;
         }
 
         private void filterToolStripMenuItem_Click(object sender, EventArgs e)
@@ -47,22 +50,55 @@ namespace ImageProcessing
 
         }
 
+        // Convert button logic
         private void button2_Click(object sender, EventArgs e)
         {
-
-            if (comboBox1.SelectedIndex == 0)
+            if (isAdvancedFilter)
             {
-                pictureBox2.Image = ImageProcessing.Processes.Copy((Bitmap)pictureBox1.Image);
+                if (currentAdvancedFilter == "static")
+                {
+                    if (pictureBox1.Image != null && pictureBox3.Image != null)
+                    {
+                        pictureBox2.Image = Processes.Subtract(
+                            (Bitmap)pictureBox1.Image,
+                            (Bitmap)pictureBox3.Image
+                        );
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select both input images for subtraction.", "Missing Image", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else if (currentAdvancedFilter == "webcam")
+                {
+                    // Placeholder for webcam subtraction logic
+                    MessageBox.Show("Webcam subtraction not implemented yet.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                return;
             }
-            else
-            {
-                pictureBox2.Image = pictureBox1.Image;
-            }
-        }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            index = comboBox1.SelectedIndex;
+            // Normal filter logic (if not advanced)
+            switch (index)
+            {
+                case 0:
+                    pictureBox2.Image = Processes.Copy((Bitmap)pictureBox1.Image);
+                    break;
+                case 1:
+                    pictureBox2.Image = Processes.GreyScale((Bitmap)pictureBox1.Image);
+                    break;
+                case 2:
+                    pictureBox2.Image = Processes.Histogram((Bitmap)pictureBox1.Image);
+                    break;
+                case 3:
+                    pictureBox2.Image = Processes.InvertColor((Bitmap)pictureBox1.Image);
+                    break;
+                case 4:
+                    pictureBox2.Image = Processes.Sepia((Bitmap)pictureBox1.Image);
+                    break;
+                default:
+                    pictureBox2.Image = pictureBox1.Image;
+                    break;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -76,13 +112,120 @@ namespace ImageProcessing
                 
                 button1.Visible = false;
 
-                //button1.Text = "";
-                //button1.FlatStyle = FlatStyle.Flat;
-                //button1.BackColor = Color.Transparent;
-                //button1.Parent = pictureBox1;
-                //button1.BringToFront();
-
             }
+        }
+
+        private void filtersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetAdvancedFilterUI();
+            index = 0;
+            label5.Text = "Basic Copy";
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetAdvancedFilterUI();
+            index = 1;
+            label5.Text = "Greyscale";
+        }
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetAdvancedFilterUI();
+            index = 2;
+            label5.Text = "Histogram";
+        }
+
+        private void colorInvertionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetAdvancedFilterUI();
+            index = 3;
+            label5.Text = "Color Inversion";
+        }
+
+        private void sepiaToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            ResetAdvancedFilterUI();
+            index = 4;
+            label5.Text = "Sepia";
+        }
+
+        private void advancedFiltersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subtractionStaticToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Width = 1120;
+            pictureBox3.Visible = true;
+            button4.Visible = true;
+            label5.Text = "Subtraction (Static)";
+            isAdvancedFilter = true;
+            currentAdvancedFilter = "static";
+        }
+
+        private void subtractionWebcamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Width = 1120;
+            pictureBox3.Visible = true;
+            button4.Visible = true;
+            label5.Text = "Subtraction (Webcam)";
+            isAdvancedFilter = true;
+            currentAdvancedFilter = "webcam";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox3.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            label1.AutoSize = true;
+            label1.Left = (ClientSize.Width - label1.Width) / 2;
+            label1.Top = (ClientSize.Height - label1.Height) / 2;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            button1.Visible = true;
+            label5.Text = "None";
+            index = 0;
+        }
+
+        private void ResetAdvancedFilterUI()
+        {
+            this.Width = 800;
+            pictureBox3.Visible = false;
+            button4.Visible = false;
+            label5.Text = "None";
+            isAdvancedFilter = false;
+            currentAdvancedFilter = "";
+            pictureBox3.Image = null;
         }
     }
 }
--- ImageProcessing/ImageProcesses.cs
+++ ImageProcessing/ImageProcesses.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +9,142 @@ namespace ImageProcessing
 {
     public static class Processes
     {
+        public static Bitmap result;
 
         public static Bitmap Copy(Bitmap src)
         {
-            return new Bitmap(src);
+            result = new Bitmap(src.Width, src.Height);
+
+            for (int y = 0; y < src.Height; y++)
+            {
+                for ( int x = 0; x < src.Width; x++)
+                {
+                    Color pixel = src.GetPixel(x, y);
+
+                    result.SetPixel(x, y, pixel);
+                }
+            }
+
+            return result;
         }
         public static Bitmap GreyScale(Bitmap src)
         {
-            return new Bitmap(src);
+            result = new Bitmap(src.Width, src.Height);
+
+            for (int y = 0; y < src.Height; y++)
+            {
+                for (int x = 0; x < src.Width; x++)
+                {
+                    Color pixel = src.GetPixel(x, y);
+
+                    // Adobe Greyscale Formula
+                    int grey = (int)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+
+                    pixel = Color.FromArgb(grey, grey, grey);
+
+                    result.SetPixel(x, y, pixel);
+                }
+            }
+
+            return result;
         }
         public static Bitmap InvertColor(Bitmap src)
         {
-            return new Bitmap(src);
+            result = new Bitmap(src.Width, src.Height);
+
+            for (int y = 0; y < src.Height; y++)
+            {
+                for (int x = 0; x < src.Width; x++)
+                {
+                    Color pixel = src.GetPixel(x, y);
+
+
+                    Color inverted = Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B);
+
+                    result.SetPixel(x, y, inverted);
+                }
+            }
+
+            return result;
         }
+
         public static Bitmap Histogram(Bitmap src)
         {
-            return new Bitmap(src);
+            int width = 256, height = 100;
+            Bitmap histImage = new Bitmap(width, height);
+            int[] hist = new int[256];
+
+            for (int y = 0; y < src.Height; y++)
+            {
+                for (int x = 0; x < src.Width; x++)
+                {
+                    Color pixel = src.GetPixel(x, y);
+
+                    //Adobe Greyscale Formula
+                    int gray = (int)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+                    hist[gray]++;
+                }
+            }
+
+            int max = hist.Max();
+            for (int x = 0; x < width; x++)
+            {
+                int value = (int)((hist[x] / (float)max) * (height - 1));
+                for (int y = height - 1; y >= height - value; y--)
+                {
+                    histImage.SetPixel(x, y, Color.Black);
+                }
+            }
+            return histImage;
         }
+
         public static Bitmap Sepia(Bitmap src)
         {
-            return new Bitmap(src);
+            result = new Bitmap(src.Width, src.Height);
+
+            for (int y = 0; y < src.Height; y++)
+            {
+                for (int x = 0; x < src.Width; x++)
+                {
+                    Color pixel = src.GetPixel(x, y);
+
+                    // Adobe Sepia Formula
+                    int r = (int)(pixel.R * 0.393 + pixel.G * 0.769 + pixel.B * 0.189);
+                    int g = (int)(pixel.R * 0.349 + pixel.G * 0.686 + pixel.B * 0.168);
+                    int b = (int)(pixel.R * 0.272 + pixel.G * 0.534 + pixel.B * 0.131);
+
+                    Color sepia = Color.FromArgb(
+                        Math.Min(255, r),
+                        Math.Min(255, g),
+                        Math.Min(255, b)
+                        );
+
+                    result.SetPixel(x, y, sepia);
+                }
+            }
+
+            return result;
+        }
+
+        public static Bitmap Subtract(Bitmap src1, Bitmap src2)
+        {
+            int width = Math.Min(src1.Width, src2.Width);
+            int height = Math.Min(src1.Height, src2.Height);
+            Bitmap result = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color c1 = src1.GetPixel(x, y);
+                    Color c2 = src2.GetPixel(x, y);
+                    int r = Math.Max(0, c1.R - c2.R);
+                    int g = Math.Max(0, c1.G - c2.G);
+                    int b = Math.Max(0, c1.B - c2.B);
+                    result.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            return result;
         }
     }
 }
